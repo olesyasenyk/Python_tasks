01072680c09ca6023ffb0403272c2c85904d0510
Olesia Falafivka <olesyasenyk@hotmail.com>
2019-09-06 23:19:32 +0300

    removed hard code

--- search_file.py
+++ search_file.py
@@ -1,12 +1,14 @@
 import os
+import sys
 
-path = 'd:\Project Documentation'
+path = sys.argv [1]
+name = sys.argv [2]
 
 files = []
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
     for file in f:
-        if 'Falafivka' in file:
+        if name in file:
             files.append(os.path.join(r, file))
 
 for f in files:
