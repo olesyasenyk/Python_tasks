b425d11242cd02421c67435186b87038a70362da
olesyasenyk <54891146+olesyasenyk@users.noreply.github.com>
2019-09-23 23:33:05 +0300

    Update 21_sticks.py

--- 21_sticks.py
+++ 21_sticks.py
@@ -1,7 +1,12 @@
 def make_move(sticks):
     if sticks%4==1:
         return 1
-    if sticks%4==2:
+    elif sticks%4==2:
         return 2
-    if sticks%4==3:
+    elif sticks%4==3:
         return 3
+    
+    
+def make_move(sticks):
+    r = sticks%4
+    return r
