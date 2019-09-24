5ad3caff42e54257cf4a3ef45891e39259d8c0c4
Olesia Falafivka <olesyasenyk@hotmail.com>
2019-09-24 22:57:29 +0300

    added unfinished loop

--- 21_sticks.py
+++ 21_sticks.py
@@ -1,7 +1,7 @@
 def make_move(sticks):
     if sticks%4==1:
         return 1
-    if sticks%4==2:
+    elif sticks%4==2:
         return 2
-    if sticks%4==3:
+    elif sticks%4==3:
         return 3
