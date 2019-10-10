a4dc324b09472a4ebb71fd7611c56f3ed3f69f7a
olesyasenyk <olesyasenyk@hotmail.com>
2019-10-11 00:36:45 +0300

    added a file

---
+++ Adam_Eve.py
@@ -0,0 +1,17 @@
+def God():
+    return Adam, Eve
+
+class Human():
+    def __init__(self):
+        pass
+    
+class Man(Human):
+    def __init__(self):
+        pass
+    
+class Woman(Human):
+    def __init__(self):
+        pass
+    
+Adam=Man()
+Eve=Woman()
